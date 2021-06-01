8f299a2549cf548ee8260ddb28c5732e303e45b7
Luka Radivoj <luka@greenloftstudios.com>
2021-06-01 16:43:21 +0200

    Started creating mazeConfig function

--- CloudScripts.js
+++ CloudScripts.js
@@ -1,24 +1,14 @@
 //MAV_Maze_Rescue PlayFab CloudScripts. 
-//A function that generates an UID. Currently it is generating an UID by incrementing the old UID. If the old UID is null, then it returns 0.
-function GenerateUID(oldUID) {
-    if (oldUID == null) {
-        return "0";
-    }
-    else {
-        return (parseInt(oldUID) + 1).toString();
-    }
-}
-//A cloud script that is used for fetching UIDs related to progress and the player inventory.
-handlers.GetUIDs = function (args, context) {
-    var request = {
-        PlayFabId: currentPlayerId,
-        Keys: ["ProgressUID", "InventoryUID"]
-    };
-    var internalData = server.GetUserInternalData(request);
-    var progressUID = internalData.Data["ProgressUID"].Value;
-    var inventoryUID = internalData.Data["InventoryUID"].Value;
-    return { ProgressUID: progressUID, InventoryUID: inventoryUID };
-};
 //Cloud script that generates Maze Configuration
 handlers.GetMazeConfig = function (args) {
+    var titleDataResult = server.GetTitleData({});
+    var data = titleDataResult.Data["Animal"];
+    var jsonObject = JSON.parse(data);
+    var size = 0;
+    for (var _i = 0, _a = Object.keys(jsonObject); _i < _a.length; _i++) {
+        var key = _a[_i];
+        size + 1;
+    }
+    var playerDataResult = server.GetPlayerStatistics({ PlayFabId: currentPlayerId });
+    var playerObject = JSON.parse(playerDataResult.Statistics["Level"]);
 };
--- CloudScripts.ts
+++ CloudScripts.ts
@@ -1,6 +1,18 @@
 //MAV_Maze_Rescue PlayFab CloudScripts. 
 
 //Cloud script that generates Maze Configuration
-handlers.GetMazeConfig=function(args){
+handlers.GetMazeConfig = function (args) {
+    var titleDataResult = server.GetTitleData({});
 
+    var data = titleDataResult.Data["Animal"];
+    var jsonObject = JSON.parse(data);
+
+    var size = 0;
+    for (var key of Object.keys(jsonObject)) {
+        size + 1;
+    }
+
+
+    var playerDataResult = server.GetPlayerStatistics({ PlayFabId: currentPlayerId });
+    var playerObject = JSON.parse(playerDataResult.Statistics["Level"]);
 }
